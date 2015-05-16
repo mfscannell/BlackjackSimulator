b3d75bd64ed5be08c2105e8e2a3ce48b0d8d8e74
mfscannell <markfscannell@gmail.com>
2015-05-16 17:42:09 -0500

    moved config file headings to separate class

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+import configFile.ConfigFileHeading;
+
 import rules.BlackjackRules;
 import enumerations.CardRank;
 import enumerations.CardSuit;
@@ -17,24 +19,6 @@ import modelObjects.BlackjackTable;
 import modelObjects.Shoe;
 
 public class Main {
-	private static final String shoeConfig = "#SHOE_PARAMETERS";
-	private static final String shoeConfigNumDecks = "#numDecks";
-	private static final String shoeConfigDeckPenetration = "#deckPenetration";
-	
-	private static final String rulesConfig = "#RULES";
-	private static final String rulesConfigMaxHands = "#maxHands";
-	private static final String rulesConfigBlackjackPayout = "#blackjackPayout";
-	private static final String rulesConfigDoubleAfterSplit = "#doubleAfterSplit";
-	private static final String rulesConfigDealerHitsSoft17 = "#dealerHitsSoft17";
-	private static final String rulesConfigResplitAces = "#resplitAces";
-	
-	private static final String tableConfig = "#TABLE_PARAMETERS";
-	private static final String tableConfigNumHands = "#numHands";
-	
-	private static final String playerConfig = "#PLAYER_PARAMETERS";
-	private static final String playerConfigNumPlayers = "#numPlayers";
-	private static final String playerConfigCardCounter = "#playerCardCounter";
-	private static final String playerConfigSeat = "#tableSeat";
 	
 	//Shoe parameters
 	private static int numDecks;
@@ -100,7 +84,6 @@ public class Main {
 			} catch (FileNotFoundException e) {
 				System.out.println("***ERROR, THAT FILE DOES NOT EXIST***");
 				invalidFile = true;
-				//e.printStackTrace();
 			}
 		} while (invalidFile);
 	}//end method acquireParameters
@@ -114,21 +97,21 @@ public class Main {
 		
 		//read the heading
 		heading = configFileReader.next();
-		if (!heading.equals(shoeConfig)) {
-			throw new InvalidConfigFileException("Expected " + shoeConfig);
+		if (!heading.equals(ConfigFileHeading.SHOE_CONFIG_HEADING)) {
+			throw new InvalidConfigFileException("Expected " + ConfigFileHeading.SHOE_CONFIG_HEADING);
 		}
 		
 		//read the number of decks
 		heading = configFileReader.next();
-		if (!heading.equals(shoeConfigNumDecks)) {
-			throw new InvalidConfigFileException("Expected " + shoeConfigNumDecks);
+		if (!heading.equals(ConfigFileHeading.NUM_DECKS)) {
+			throw new InvalidConfigFileException("Expected " + ConfigFileHeading.NUM_DECKS);
 		}
 		numDecks = configFileReader.nextInt();
 		
 		//read the deck penetration
 		heading = configFileReader.next();
-		if (!heading.equals(shoeConfigDeckPenetration)) {
-			throw new InvalidConfigFileException("Expected " + shoeConfigDeckPenetration);
+		if (!heading.equals(ConfigFileHeading.DECK_PENETRATION_IN_PERCENT)) {
+			throw new InvalidConfigFileException("Expected " + ConfigFileHeading.DECK_PENETRATION_IN_PERCENT);
 		}
 		deckPenetration = configFileReader.nextInt();
 		
@@ -146,49 +129,43 @@ public class Main {
 	private static void setUpRules() throws InvalidConfigFileException {
 		String heading;
 		
-		//read the heading
 		heading = configFileReader.next();
-		if (!heading.equals(rulesConfig)) {
-			throw new InvalidConfigFileException("Expected " + rulesConfig);
+		if (!heading.equals(ConfigFileHeading.RULES_CONFIG_HEADING)) {
+			throw new InvalidConfigFileException("Expected " + ConfigFileHeading.RULES_CONFIG_HEADING);
 		}
 		
-		//read the number of max hands after player splits/resplits
 		heading = configFileReader.next();
-		if (!heading.equals(rulesConfigMaxHands)) {
-			throw new InvalidConfigFileException("Expected " + rulesConfigMaxHands);
+		if (!heading.equals(ConfigFileHeading.MAX_HANDS_AFTER_RESPLITS)) {
+			throw new InvalidConfigFileException("Expected " + ConfigFileHeading.MAX_HANDS_AFTER_RESPLITS);
 		}
 		maxHands = configFileReader.nextInt();
 		if (maxHands < 1 || maxHands > 4) {
 			throw new InvalidConfigFileException("Maximum number of hands must be between 1 and 4");
 		}
 		
-		//read the blackjack payout
 		heading = configFileReader.next();
-		if (!heading.equals(rulesConfigBlackjackPayout)) {
-			throw new InvalidConfigFileException("Expected " + rulesConfigBlackjackPayout);
+		if (!heading.equals(ConfigFileHeading.BLACKJACK_PAYOUT)) {
+			throw new InvalidConfigFileException("Expected " + ConfigFileHeading.BLACKJACK_PAYOUT);
 		}
 		blackjackPayout = configFileReader.nextDouble();
 		
-		//read the rules for double after split
 		heading = configFileReader.next();
-		if (!heading.equals(rulesConfigDoubleAfterSplit)) {
-			throw new InvalidConfigFileException("Expected " + rulesConfigDoubleAfterSplit);
+		if (!heading.equals(ConfigFileHeading.DOUBLE_AFTER_SPLIT)) {
+			throw new InvalidConfigFileException("Expected " + ConfigFileHeading.DOUBLE_AFTER_SPLIT);
 		}
 		heading = configFileReader.next();
 		doubleAfterSplit = Boolean.parseBoolean(heading);
 		
-		//read the rules for dealer hits soft 17
 		heading = configFileReader.next();
-		if (!heading.equals(rulesConfigDealerHitsSoft17)) {
-			throw new InvalidConfigFileException("Expected " + rulesConfigDealerHitsSoft17);
+		if (!heading.equals(ConfigFileHeading.DEALER_HITS_SOFT_17)) {
+			throw new InvalidConfigFileException("Expected " + ConfigFileHeading.DEALER_HITS_SOFT_17);
 		}
 		heading = configFileReader.next();
 		dealerHitsSoft17 = Boolean.parseBoolean(heading);
 		
-		//read the rules for resplitting aces
 		heading = configFileReader.next();
-		if (!heading.equals(rulesConfigResplitAces)) {
-			throw new InvalidConfigFileException("Expected " + rulesConfigResplitAces);
+		if (!heading.equals(ConfigFileHeading.RESPLIT_ACES)) {
+			throw new InvalidConfigFileException("Expected " + ConfigFileHeading.RESPLIT_ACES);
 		}
 		heading = configFileReader.next();
 		resplitAces = Boolean.parseBoolean(heading);
@@ -209,16 +186,14 @@ public class Main {
 	private static void setUpTable() throws InvalidConfigFileException {
 		String heading;
 		
-		//read the heading
 		heading = configFileReader.next();
-		if (!heading.equals(tableConfig)) {
-			throw new InvalidConfigFileException("Expected " + tableConfig);
+		if (!heading.equals(ConfigFileHeading.TABLE_PARAMETERS_HEADING)) {
+			throw new InvalidConfigFileException("Expected " + ConfigFileHeading.TABLE_PARAMETERS_HEADING);
 		}
 		
-		//read the number of hands to simulate
 		heading = configFileReader.next();
-		if (!heading.equals(tableConfigNumHands)) {
-			throw new InvalidConfigFileException("Expected " + tableConfigNumHands);
+		if (!heading.equals(ConfigFileHeading.NUM_HANDS_TO_SIMULATE)) {
+			throw new InvalidConfigFileException("Expected " + ConfigFileHeading.NUM_HANDS_TO_SIMULATE);
 		}
 		numHands = configFileReader.nextInt();
 		
@@ -240,16 +215,14 @@ public class Main {
 	private static void setUpPlayers() throws InvalidConfigFileException {
 		String heading;
 		
-		//read the heading
 		heading = configFileReader.next();
-		if (!heading.equals(playerConfig)) {
-			throw new InvalidConfigFileException("Expected " + playerConfig);
+		if (!heading.equals(ConfigFileHeading.PLAYER_PARAMETERS_HEADING)) {
+			throw new InvalidConfigFileException("Expected " + ConfigFileHeading.PLAYER_PARAMETERS_HEADING);
 		}
 		
-		//read the number of players
 		heading = configFileReader.next();
-		if (!heading.equals(playerConfigNumPlayers)) {
-			throw new InvalidConfigFileException("Expected " + playerConfigNumPlayers);
+		if (!heading.equals(ConfigFileHeading.NUM_PLAYERS)) {
+			throw new InvalidConfigFileException("Expected " + ConfigFileHeading.NUM_PLAYERS);
 		}
 		numPlayers = configFileReader.nextInt();
 		if (numPlayers < BlackjackTable.MIN_PLAYERS || numPlayers > BlackjackTable.MAX_PLAYERS) {
@@ -259,15 +232,15 @@ public class Main {
 		//create the players
 		for (int i = 0; i < numPlayers; i++) {
 			heading = configFileReader.next();
-			if (!heading.equals(playerConfigSeat)) {
-				throw new InvalidConfigFileException("Expected " + playerConfigSeat);
+			if (!heading.equals(ConfigFileHeading.SEAT_AT_TABLE)) {
+				throw new InvalidConfigFileException("Expected " + ConfigFileHeading.SEAT_AT_TABLE);
 			}
 			heading = configFileReader.next();
 			int seat = Integer.parseInt(heading);
 			
 			heading = configFileReader.next();
-			if (!heading.equals(playerConfigCardCounter)) {
-				throw new InvalidConfigFileException("Expected " + playerConfigCardCounter);
+			if (!heading.equals(ConfigFileHeading.PLAYER_CARD_COUNTER)) {
+				throw new InvalidConfigFileException("Expected " + ConfigFileHeading.PLAYER_CARD_COUNTER);
 			}
 			heading = configFileReader.next();
 			boolean cardCounter = Boolean.parseBoolean(heading);
